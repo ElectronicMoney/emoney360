b0da5f9f7f7d7566d3326bbc5f9a6761ee9bc58b
Emeka Augustine <cafafanscoders@gamil.com>
2019-02-03 07:53:55 -0500

    Created all migrations and pivot table

--- database/migrations/2019_02_03_101241_create_products_table.php
+++ database/migrations/2019_02_03_101241_create_products_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\Models\Product;
 
 class CreateProductsTable extends Migration
 {
@@ -15,7 +16,15 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('seller_id');
+            $table->string('name');
+            $table->text('description');
+            $table->integer('quantity')->unsigned();
+            $table->string('status')->default(Product::UNAVAILABLE_PRODUCT);
+            $table->string('image');
             $table->timestamps();
+
+            $table->foreign('seller_id')->referenecse('id')->on('users');
         });
     }
 
