f4a86319080de7c864389c217656a01816dd78b8
Emeka Augustine <cafafanscoders@gamil.com>
2019-02-03 12:57:44 -0500

    Implemented the Operations for UserController

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -15,17 +16,10 @@ class UserController extends Controller
     public function index()
     {
         //
+        $users = User::all();
+        return response()->json(['data' => $users], 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -35,7 +29,25 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validate the input
+        $rules = [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ];
+
+        $this->validate($request, $rules);
+
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+
+        $data['verified'] = User::UNVERIFIED_USER;
+        $data['verification_token'] = User::generateVerificationCode();
+        $data['admin'] = User::REGULAR_USER;
+
+        //Then create the user
+        $user = User::create($data);
+        return response()->json(['data' => $user], 201);
     }
 
     /**
@@ -47,18 +59,10 @@ class UserController extends Controller
     public function show($id)
     {
         //
+        $user = User::findOrFail($id);
+        return response()->json(['data' => $user], 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
@@ -69,7 +73,49 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::findOrFail($id);
+        //validate the input
+        $rules = [
+            'email' => 'email|unique:users,email,' . $user->id,
+            'password' => 'min:6|confirmed',
+            'admin' => 'in'. User::ADMIN_USER . ',' . User::REGULAR_USER,
+        ];
+
+        //check if the request has name
+        if ($request->has('name')) {
+            $user->name = $request->name;
+        }
+
+         //check if the request has email and user email is not request email
+        if ($request->has('email') && $user->email != $request->email) {
+            $user->verified = User::UNVERIFIED_USER;
+            $user->verification_token = User::generateVerificationCode();
+            $user->email = bcrypt($request->email);
+        }
+
+         //check if the request has password
+        if ($request->has('password')) {
+            $user->password = $request->password;
+        }
+
+         //check if the request has admin
+        if ($request->has('admin')) {
+            if (!$user->isVerified()) {
+                return response()->json(['error' => 'Only the verified users can modify the admin field', 'code' => 409], 409);
+            }
+
+            $user->admin = $request->admin;
+        }
+
+        //check if the user actually changed new things
+        if(!$user->isDirty()) {
+            return response()->json(['error' => 'You need to specify a different value to update', 'code' => 422], 422);
+        }
+
+        //save if everything are ok and return a responce
+        $user->save();
+        return response()->json(['data' => $user], 200);
+
     }
 
     /**
@@ -80,6 +126,11 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        //check if the user exist
+        $user = User::findOrFail($id);
+        //delete the user
+        $user->delete();
+        //return a response
+        return response()->json(['data' => $user], 200);
     }
 }
